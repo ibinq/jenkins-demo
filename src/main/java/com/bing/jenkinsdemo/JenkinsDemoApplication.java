1c879f6aa13326fbda4b336fe028a0243a7b632a
ZhouBing <5118757+ZhouBing007@user.noreply.gitee.com>
2020-10-10 06:58:26 +0800

    nothing

--- src/main/java/com/bing/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/bing/jenkinsdemo/JenkinsDemoApplication.java
@@ -1,13 +1,27 @@
 package com.bing.jenkinsdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController()
 public class JenkinsDemoApplication {
+
+    @Value("${zhoubing.name}")
+    private String name;
+
+    @Value("${zhoubing.age}")
+    private Integer age;
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
     }
 
+    @GetMapping("hi")
+    public String hi(){
+        return  (name+"--"+age);
+    }
 }
